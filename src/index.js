4b32f66b3b1d286e117ac14c8140a402feb0d4d6
aadwilli <aadwilli15@gmail.com>
2017-07-10 12:21:50 -0400

    added friend functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header';
+import FriendForm from './FriendForm';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<div className="row">
+		<App>
+			<Header />
+			<FriendForm />
+		</App>
+	</div>, 
+	document.getElementById('root'));
 registerServiceWorker();
